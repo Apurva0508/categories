62e4dc9589fc2dce5e85cfd3ba102273c929823e
Apurva Padwal <apadwal@motionusa.com>
2026-03-31 15:27:11 -0400

    Configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/category/',
+  base: '/categories/',
   plugins: [react(), tailwindcss()],
 })
